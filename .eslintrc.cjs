c9dc8fad203ed3ed4014f4cfec38aa383cb76c0b
mchornomaz <chornomaz.maksym@gmail.com>
2023-12-28 23:45:15 +0200

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    "ignorePatterns": [".eslintrc.cjs", "jest.config.js", "postcss.config.js"],
+    "overrides": [
+        {
+            "env": {
+                "node": true
+            },
+            "files": [
+                ".eslintrc.{js,cjs}"
+            ],
+            "parserOptions": {
+                "sourceType": "script"
+            }
+        }
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <>
+      <p className='text-green-500 text-3xl'>Hello World</p>
+    </>
+  )
+}
+
+export default App
